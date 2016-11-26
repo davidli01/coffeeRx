2ed03466a5f722aedda7672dc9ba00e04c997c1d
David Li <jdavidli01@outlook.com>
2016-11-26 12:17:06 -0500

    server testing

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var app = express(); //set reference to express dependency execution
 
 app.set('port', 3000); //set express application variable port to 3000
 
+app.get('/', function(request, response){ //hhtp method / get method
+	console.log("GET the Homepage");
+	response.status(404).send("Hello World");
+});
+
 var server = app.listen(app.get('port'), function() { //callback function
 	var port = server.address().port;
 	console.log('Listening to Port ' + port);	
